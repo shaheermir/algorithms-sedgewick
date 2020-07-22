ba357eb1372a37b5d9e7418a819ac3fd447bc8f1
Shaheer Mir <shaheermir0@gmail.com>
2020-07-22 18:12:29 -0400

    first commit

---
+++ src/chapter1/section1/Exercise1.java
@@ -0,0 +1,16 @@
+package chapter1.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise1 {
+
+  public static void main(String[] args) {
+    int resultA = (0 + 15) / 2;
+    double resultB = 2.0e-6 * 100000000.1;
+    boolean resultC = true && false || true && true;
+
+    StdOut.println("a) " + resultA);
+    StdOut.println("b) " + resultB);
+    StdOut.println("c) " + resultC);
+  }
+}
