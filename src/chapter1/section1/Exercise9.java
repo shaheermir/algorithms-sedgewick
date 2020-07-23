6e41a6087416222a12351e202e2d867358100c4d
Shaheer Mir <shaheermir0@gmail.com>
2020-07-22 21:21:27 -0400

    add ex9 and ex13

---
+++ src/chapter1/section1/Exercise9.java
@@ -0,0 +1,21 @@
+package chapter1.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Exercise9 {
+    public static String intToBinary(int n) {
+        String result = "";
+
+        while (n > 0) {
+            result = n % 2 + result;
+            n = n / 2;
+            System.out.println(n);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        StdOut.println(intToBinary(4));
+        StdOut.println("Expected: 100");
+    }
+}
